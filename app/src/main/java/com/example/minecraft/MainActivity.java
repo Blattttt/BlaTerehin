796c9455e092c4e621188c116ac8105601b22f0f
Arseny <terexin-2003@mail.ru>
2021-10-15 17:05:24 +0300

    создал список

--- app/src/main/java/com/example/minecraft/MainActivity.java
+++ app/src/main/java/com/example/minecraft/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.minecraft;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] countries = {""};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView countriesList = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, countries);
+        countriesList.setAdapter(adapter);
+        countriesList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                String indexItem= countries[position];
+            }
+        });
     }
 }
