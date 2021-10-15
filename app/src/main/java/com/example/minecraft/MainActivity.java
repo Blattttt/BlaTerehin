b91bfba0eb8a9307fdebad21cd8b1879658186e9
Arseny <terexin-2003@mail.ru>
2021-10-15 18:05:54 +0300

    инициализировал массив для списка создал второй активити написал передачу параметра через intent

--- app/src/main/java/com/example/minecraft/MainActivity.java
+++ app/src/main/java/com/example/minecraft/MainActivity.java
@@ -2,31 +2,46 @@ package com.example.minecraft;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    String[] countries = {""};
+    String[] countries = {"Кирка", "Лопата", "Мотыга", "Топор", "Шлем", "Нагрудник", "Штаны", "Ботинки", "Блок"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ListView countriesList = (ListView) findViewById(R.id.listView);
-        ArrayAdapter<String> adapter = new ArrayAdapter(this,
+        ArrayAdapter adapter = new ArrayAdapter(this,
                 android.R.layout.simple_list_item_1, countries);
         countriesList.setAdapter(adapter);
         countriesList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
 
+
             @Override
             public void onItemClick(AdapterView<?> parent, View v, int position, long id)
             {
-                String indexItem= countries[position];
-            }
+               intentString( countries[position]);
+                }
+
         });
+    }
+
+    public void intentString(String index){
+        try {
+            Intent intent = new Intent(this,SecondActivity.class);
+            intent.putExtra("id", index);
+            startActivity(intent);
+        }catch (Exception e) {
+            Toast.makeText(this, "Ошибка", Toast.LENGTH_LONG).show();
+        }
     }
 }
