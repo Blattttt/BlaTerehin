e2145de00f4d058ee25eca703b40cc0db9675647
Arseny <terexin-2003@mail.ru>
2021-10-19 15:47:27 +0300

    написана логика второй страницы

--- app/src/main/java/com/example/minecraft/SecondActivity.java
+++ app/src/main/java/com/example/minecraft/SecondActivity.java
@@ -2,7 +2,13 @@ package com.example.minecraft;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,6 +16,18 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        String indexItem = getIntent().getExtras().getString("id");
+        String name = getIntent().getExtras().getString("id");
+        TextView text = findViewById(R.id.textSecond);
+        text.setText(name);
+        ImageView imageView = findViewById(R.id.imageSecond);
+        String filename = name + ".png";
+        try(InputStream inputStream = getApplicationContext().getAssets().open(filename)){
+            Drawable drawable = Drawable.createFromStream(inputStream, null);
+            imageView.setImageDrawable(drawable);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
